1a792354db68c152d4060afb4dad089e500e096c
imrizwan <razzyshaikh@gmail.com>
2019-08-24 18:44:38 +0500

    Template Backup

---
+++ template/src/data/tags.js
@@ -0,0 +1,22 @@
+export const tags = {
+  sectionTitle: '',
+  icon: null,
+  list: [
+    'Javascript',
+    'ReactJS',
+    'ReactNative',
+    'Redux',
+    'Webpack',
+    'Npm/Yarn',
+    'Git',
+    'F#',
+    'ASP.NETMVC',
+    'PHP',
+    'SOLID PRINCIPLES',
+    'XAMARIN',
+    'LINQ',
+    'N-TIER ARCHITECTURE',
+    'SOA ARCHITECTURE']
+};
+
+export default tags;
